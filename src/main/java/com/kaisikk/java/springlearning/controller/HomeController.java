ab65b5ca5278c3af1f1c31e5a545bf467b50d9eb
Kaisikk <Kosikov.Ilya@mail.ru>
2024-03-26 21:33:01 +0400

    Создание формы и контроллера для сохранения объекта.

--- src/main/java/com/kaisikk/java/springlearning/controller/HomeController.java
+++ src/main/java/com/kaisikk/java/springlearning/controller/HomeController.java
@@ -1,10 +1,12 @@
 package com.kaisikk.java.springlearning.controller;
 
+import com.kaisikk.java.springlearning.model.Book;
 import com.kaisikk.java.springlearning.repo.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class HomeController {
@@ -20,7 +22,14 @@ public class HomeController {
     public String getIndex(Model model) {
 
         model.addAttribute("books", bookRepository.findAll());
+        model.addAttribute("newbook", new Book());
 
         return "index";
+    }
+
+    @PostMapping
+    public String createBook(Book book) {
+        bookRepository.save(book);
+        return "redirect:/";
     }
 }
