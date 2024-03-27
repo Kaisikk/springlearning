494d91179705b5beef0e3a1bb64a3508328c9822
Kaisikk <Kosikov.Ilya@mail.ru>
2024-03-27 22:43:14 +0400

    Добавление контроллеров для изменения объектов

--- src/main/java/com/kaisikk/java/springlearning/controller/HomeController.java
+++ src/main/java/com/kaisikk/java/springlearning/controller/HomeController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -42,4 +43,39 @@ public class HomeController {
         bookRepository.save(book);
         return "redirect:/";
     }
+
+    @GetMapping("/{id}/show")
+    public String showById(@PathVariable("id") Long id, Model model) {
+        if (bookRepository.findById(id).isPresent()) {
+            model.addAttribute("book", bookRepository.findById(id).get());
+            return "show";
+        }
+        return "show";
+    }
+
+    @GetMapping("/{id}/delete")
+    public String deleteById(@PathVariable("id") Long id) {
+
+        bookRepository.deleteById(id);
+        return "redirect:/";
+    }
+
+
+    @GetMapping("/{id}/edit")
+    public String editForm(@PathVariable("id") Long id, Model model) {
+        if (bookRepository.findById(id).isPresent()) {
+            model.addAttribute("book", bookRepository.findById(id).get());
+            return "edit";
+        }
+        model.addAttribute("book", new Book());
+        return "edit";
+    }
+
+    @PostMapping("/{id}")
+    public String updateBook(@PathVariable("id") Long id, Book book) {
+        bookRepository.save(book);
+        return "redirect:/";
+    }
+
+
 }
